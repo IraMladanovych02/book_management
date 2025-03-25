b20e9d974c80e9a5e3beb3f25fc3276962a4ae29
Ira <iramladanovych@gmail.com>
2025-03-25 11:42:11 +0200

    Set up alembic configurations

--- db/engine.py
+++ db/engine.py
@@ -16,7 +16,7 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
